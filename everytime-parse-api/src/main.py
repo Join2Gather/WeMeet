bbc07ab89b4198952c9a5d476f902614fa0bc679
Gnu-Kenny <qkr0677@gmail.com>
2021-08-23 20:50:38 +0900

    feat: 상대경로 import 해결
    Co-authored-by: jiho lee <optional.int@kakao.com>

--- everytime-parse-api/src/main.py
+++ everytime-parse-api/src/main.py
@@ -1,16 +1,23 @@
+if __name__ == '__main__':
+    from sys import path
+    import os
+    path.append(os.path.dirname(__file__))
+
 from typing import Optional
 from parse import parse_img
 from fastapi import FastAPI, File, UploadFile, Response
+from fastapi.encoders import jsonable_encoder as to_json
+from fastapi.responses import JSONResponse
 import uvicorn
 
 app = FastAPI()
 
 @app.post("/everytime")
-async def parse_everytime(image: UploadFile = File(...)):
+async def parse_everytime(image: UploadFile = File(...)) -> Response:
     if image.content_type != 'image/png':
-        return Response({'error': 'file is not image file.'}, status_code=400)
+        return JSONResponse(to_json({'error': 'file is not image file.'}), status_code=400)
     result = parse_img(img_file=image.file)
-    return result
+    return JSONResponse(to_json({'result': result}), status_code=200)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8009)
