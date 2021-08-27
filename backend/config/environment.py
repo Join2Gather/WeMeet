688c4755fd2bfddb24a54aeeed5aa78906c4791a
Gnu-Kenny <qkr0677@gmail.com>
2021-08-27 17:56:47 +0900

    refact: get_secret 반환값 명시적 에러 제거
    Co-authored-by: jiho lee optional.int@kakao.com

--- backend/config/environment.py
+++ backend/config/environment.py
@@ -6,14 +6,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 secret_file = os.path.join(BASE_DIR, 'secrets.json') # secrets.json 파일 위치를 명시
 
-with open(secret_file) as f:
-    secrets = json.loads(f.read())
+secrets = {}
+if os.path.isfile(secret_file):
+    with open(secret_file) as f:
+        secrets = json.loads(f.read())
 
-def get_secret(setting, secrets=secrets):
-    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
-    try:
-        return secrets[setting]
-    except KeyError:
-        error_msg = "Set the {} environment variable".format(setting)
-        raise ImproperlyConfigured(error_msg)
-
+def get_secret(key: str, secrets: dict = secrets) -> any:
+    return secrets.get(key, None)
