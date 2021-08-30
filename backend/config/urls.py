fb28207f660b1c8f3cba60c1cc4490e80a0e669b
jiho lee <jhlee@autocrypt.io>
2021-08-30 12:41:48 +0900

    feat: drf-yasg 도입

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,7 +15,29 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path(r'^swagger(?P<format>\.json|\.yaml)$',
+         schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path(r'^swagger/$', schema_view.with_ui('swagger',
+                                            cache_timeout=0), name='schema-swagger-ui'),
+    path(r'^redoc/$', schema_view.with_ui('redoc',
+                                          cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
 ]
