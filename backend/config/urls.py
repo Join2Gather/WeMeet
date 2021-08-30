e06b495fac6c034f656615b230f6421ddfe0a7eb
jiho lee <optional.int@kakao.com>
2021-08-30 14:00:04 +0900

    fix: 정규식 def-yasg url 수정

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -33,11 +33,11 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path(r'^swagger(?P<format>\.json|\.yaml)$',
+    path(r'swagger(?P<format>\.json|\.yaml)$',
          schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path(r'^swagger/$', schema_view.with_ui('swagger',
-                                            cache_timeout=0), name='schema-swagger-ui'),
-    path(r'^redoc/$', schema_view.with_ui('redoc',
-                                          cache_timeout=0), name='schema-redoc'),
+    path('swagger/', schema_view.with_ui('swagger',
+                                         cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+                                       cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
 ]
