43ff4ff11e4d9f409d3f4e9796cc8547161326a3
Gnu-Kenny <qkr0677@gmail.com>
2021-08-31 11:51:00 +0900

    fix: callback 경로에서 파싱되는 클래스 변경
    Co-authored-by: jiho lee <optional.int@kakao.com>

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('accounts/kakao/login/', views.KakaoLogin, name='kakao_login'),
-    path('accounts/kakao/callback/', views.KakaoCallback.as_view, name='kakao_callback'),
+    path('accounts/kakao/callback/', views.KakaoCallbackView, name='kakao_callback'),
     path('accounts/kakao/login/finish/', views.KakaoLoginToDjango.as_view(), name='kakao_login_todjango'),
     
 ]
