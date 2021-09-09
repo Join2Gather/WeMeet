31fac17997582b6e9ccfbea9553b553725a7cc11
ww8007 <wshmin@naver.com>
2021-09-10 03:36:08 +0900

    Fix: remove Logout not-used

--- frontend/src/screens/MainNavigator.tsx
+++ frontend/src/screens/MainNavigator.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import {createDrawerNavigator} from '@react-navigation/drawer';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import Login from './Login';
-import SignUp from './SignUp';
 import TabNavigator from './TabNavigator';
 import DrawerContent from './DrawerContent';
 
 const Drawer = createDrawerNavigator();
 
 export default function MainNavigator() {
-  return (
-    <Drawer.Navigator
-      screenOptions={{headerShown: false}}
-      drawerContent={props => <DrawerContent {...props} />}>
-      <Drawer.Screen name="Login" component={Login} />
-      <Drawer.Screen name="SignUp" component={SignUp} />
-      <Drawer.Screen
-        name="TabNavigator"
-        component={TabNavigator}
-        options={{title: 'Home'}}
-      />
-    </Drawer.Navigator>
-  );
+	return (
+		<Drawer.Navigator
+			screenOptions={{ headerShown: false }}
+			drawerContent={(props) => <DrawerContent {...props} />}
+		>
+			<Drawer.Screen name="Login" component={Login} />
+			<Drawer.Screen
+				name="TabNavigator"
+				component={TabNavigator}
+				options={{ title: 'Home' }}
+			/>
+		</Drawer.Navigator>
+	);
 }
