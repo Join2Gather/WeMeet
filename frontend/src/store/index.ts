6c6795f64045a4a26b2e9dd2f7688a7d92cf0d5a
ww8007 <wshmin@naver.com>
2021-09-10 03:42:03 +0900

    Feat: add login reducer to rootReducer

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,20 +1,21 @@
-import {combineReducers} from 'redux';
-import {all} from 'redux-saga/effects';
-import {persistReducer} from 'redux-persist';
+import { combineReducers } from 'redux';
+import { all } from 'redux-saga/effects';
+import { persistReducer } from 'redux-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import counter from './counter';
-import sample, {sampleSaga} from './sample';
+import sample, { sampleSaga } from './sample';
 import loading from './loading';
+import login from './login';
 // import storageSession from 'redux-persist/lib/storage/session';
 const persistConfig = {
-  key: 'root',
-  storage: AsyncStorage,
-  whitelist: [''],
+	key: 'root',
+	storage: AsyncStorage,
+	whitelist: [''],
 };
-const rootReducer = combineReducers({counter, sample, loading});
+const rootReducer = combineReducers({ counter, sample, loading, login });
 export default persistReducer(persistConfig, rootReducer);
 export type RootState = ReturnType<typeof rootReducer>;
 
 export function* rootSaga() {
-  yield all([sampleSaga()]);
+	yield all([sampleSaga()]);
 }
