f783397bba23b3fb7c44bc79c011d79a6efdc4f1
DPS0340 <optional.int@kakao.com>
2021-09-17 20:42:51 +0900

    feat: implement ProfileView GET request

--- backend/profiles/urls.py
+++ backend/profiles/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    
+    path('<int:user>/profiles/<int:profile>/', views.ProfileView.as_view(), name='profile'),
 ]
--- backend/profiles/views.py
+++ backend/profiles/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.http.response import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from config.models import Profiles
+from config.models import ClubEntries
+from config.models import ProfileDates
 # Create your views here.
+class ProfileView(APIView):
+    def get(self, request: Request, user: int, profile: int):
+        profile = Profiles.objects.filter(id=profile, user=user)
+        if not profile.exists():
+            return JsonResponse({})
+        profile = profile.values()[0]
+        profile['clubs'] = [entry.club_id for entry in ClubEntries.objects.filter(profile=profile.get('id'))]
+        profile['dates'] = [profile_date for profile_date in ProfileDates.objects.filter(profile=profile.get('id'))]
+        return JsonResponse(profile)
