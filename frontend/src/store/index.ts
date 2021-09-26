8ec1a13ec8c13a9b9159c2f4885a73f2cba6d2cb
ww8007 <wshmin@naver.com>
2021-09-26 23:05:41 +0900

    Feat: add team→rootReducer, teamSaga→rootReducer

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -2,20 +2,20 @@ import { combineReducers } from 'redux';
 import { all } from 'redux-saga/effects';
 import { persistReducer } from 'redux-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import counter from './counter';
-import sample, { sampleSaga } from './sample';
+// import sample, { sampleSaga } from './sample';
 import loading from './loading';
 import login from './login';
+import team, { teamSaga } from './team';
 // import storageSession from 'redux-persist/lib/storage/session';
 const persistConfig = {
 	key: 'root',
 	storage: AsyncStorage,
-	whitelist: [''],
+	blacklist: [],
 };
-const rootReducer = combineReducers({ counter, sample, loading, login });
+const rootReducer = combineReducers({ loading, login, team });
 export default persistReducer(persistConfig, rootReducer);
 export type RootState = ReturnType<typeof rootReducer>;
 
 export function* rootSaga() {
-	yield all([sampleSaga()]);
+	yield all([teamSaga()]);
 }
