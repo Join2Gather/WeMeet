bd648ad7613a5ffa1a5768b38575a8bb61bbd525
ww8007 <wshmin@naver.com>
2021-09-26 23:06:41 +0900

    Feat: add team store with reduxSaga, Slice

---
+++ frontend/src/store/team.ts
@@ -0,0 +1,44 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import createRequestSaga from '../hooks/createRequestSaga';
+import * as api from '../lib/api/team';
+import { takeLatest } from 'redux-saga/effects';
+import { createAction } from 'redux-actions';
+import type { requestTeamAPI, responseTeamAPI, team } from '../interface';
+
+const POST_TEAM = 'team/POST_TEAM';
+
+export const postTeamName = createAction(
+	POST_TEAM,
+	(data: requestTeamAPI) => data
+);
+
+const postTeamSaga = createRequestSaga(POST_TEAM, api.postTeamName);
+
+export function* teamSaga() {
+	yield takeLatest(POST_TEAM, postTeamSaga);
+}
+
+const initialState: team = {
+	uri: '',
+	date: {},
+	name: '',
+};
+
+export const teamSlice = createSlice({
+	name: 'team',
+	initialState,
+	reducers: {
+		POST_TEAM_SUCCESS: (state, action: PayloadAction<responseTeamAPI>) => {
+			state.date = action.payload.date;
+			state.uri = action.payload.uri;
+		},
+		inputTeamName: (state, action: PayloadAction<string>) => {
+			state.name = action.payload;
+		},
+	},
+	extraReducers: {},
+});
+
+export const { inputTeamName } = teamSlice.actions;
+
+export default teamSlice.reducer;
