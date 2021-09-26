96678a0ea6429cb99c6fc5c9896b67d59f808d9e
ww8007 <wshmin@naver.com>
2021-09-26 23:04:41 +0900

    Fix: save response data with redux

--- frontend/src/screens/Login/SocialWebview.tsx
+++ frontend/src/screens/Login/SocialWebview.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/native';
 import React, { Component } from 'react';
 import { WebView } from 'react-native-webview';
 import { useDispatch } from 'react-redux';
@@ -8,27 +9,33 @@ import { getSocialLogin } from '../../store/login';
 
 export function SocialWebview({ closeSocialModal, source }: any) {
 	const dispatch = useDispatch();
+	const navigation = useNavigation();
 	const INJECTED_JAVASCRIPT =
 		'(function() {if(window.document.getElementsByTagName("pre").length>0){window.ReactNativeWebView.postMessage((window.document.getElementsByTagName("pre")[0].innerHTML));}})();';
 
 	const _handleMessage = async (event: any) => {
-		console.log(JSON.parse(event.nativeEvent.data));
 		let result = JSON.parse(event.nativeEvent.data);
-		let success = result.message;
-		if (success) {
-			const data = {
-				meetingID: result.id, // pk
-				meetingName: result.name,
-				userToken: result.Authorization,
-			};
-			// let userToken = result.Authorization;
-			try {
-				dispatch(getSocialLogin(data));
-			} catch (e) {
-				console.log(e);
-			}
+		console.log(typeof result);
+
+		const data = {
+			token: result.access_token,
+			name: result.profiles[0].name,
+			user: result.profiles[0].user,
+			id: result.profiles[0].id,
+			clubs: result.profiles[0].clubs,
+			dates: result.profiles[0].dates,
+		};
+		// let userToken = result.Authorization;
+		console.log(data);
+
+		try {
+			dispatch(getSocialLogin(data));
+		} catch (e) {
+			console.log(e);
 		}
+
 		closeSocialModal();
+		navigation.navigate('TabNavigator');
 	};
 
 	return (
