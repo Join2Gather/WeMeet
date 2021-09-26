70f2431cf8baf06380e3b4cab2d8cadab420fe3c
ww8007 <wshmin@naver.com>
2021-09-26 23:07:07 +0900

    Design: add font, fix color

--- frontend/src/theme/NavigationHeader.tsx
+++ frontend/src/theme/NavigationHeader.tsx
@@ -1,43 +1,53 @@
 import React from 'react';
-import type {FC, ReactNode} from 'react';
-import {StyleSheet} from 'react-native';
-import {View, Text} from './navigation';
-import type {StyleProp, ViewStyle, TextStyle} from 'react-native';
+import type { FC, ReactNode } from 'react';
+import { StyleSheet } from 'react-native';
+import { View, Text } from './navigation';
+import type { StyleProp, ViewStyle, TextStyle } from 'react-native';
+import { Colors } from 'react-native-paper';
 
 export type NavigationHeaderProps = {
-  title?: string;
-  Left?: () => ReactNode;
-  Right?: () => ReactNode;
-  viewStyle?: StyleProp<ViewStyle>;
-  titleStyle?: StyleProp<TextStyle>;
+	title?: string;
+	Left?: () => ReactNode;
+	Right?: () => ReactNode;
+	viewStyle?: StyleProp<ViewStyle>;
+	titleStyle?: StyleProp<TextStyle>;
 };
 
 export const NavigationHeader: FC<NavigationHeaderProps> = ({
-  title,
-  Left,
-  Right,
-  viewStyle,
-  titleStyle,
+	title,
+	Left,
+	Right,
+	viewStyle,
+	titleStyle,
 }) => {
-  return (
-    <View style={[styles.view, viewStyle]}>
-      {Left && Left()}
-      <View style={styles.flex}>
-        <Text style={[styles.title, titleStyle]}>{title}</Text>
-      </View>
-      {Right && Right()}
-    </View>
-  );
+	return (
+		<View style={[styles.view, viewStyle]}>
+			{Left && Left()}
+			<View style={styles.flex}>
+				<Text style={[styles.title, titleStyle]}>{title}</Text>
+			</View>
+			{Right && Right()}
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  view: {
-    width: '100%',
-    padding: 5,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-  title: {fontSize: 20, fontWeight: '500', textAlign: 'center'},
-  flex: {flex: 1, backgroundColor: 'transparent'},
+	view: {
+		width: '100%',
+		padding: 9,
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		backgroundColor: '#33aafc',
+	},
+	title: {
+		marginTop: 3,
+		fontSize: 20,
+		fontWeight: '300',
+		textAlign: 'center',
+		fontFamily: 'SCDream3',
+		letterSpacing: -2,
+		color: '#fff',
+	},
+	flex: { flex: 1, backgroundColor: 'transparent' },
 });
