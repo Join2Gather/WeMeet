e8ffa65e9a14ab89d6fae8c659acd72a24378553
Gnu-Kenny <qkr0677@gmail.com>
2021-09-27 16:35:14 +0900

    feat: install dependency on run.sh

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path('kakao/login/', views.KakaoLoginView.as_view(), name='kakao_login'),
     path('kakao/callback/', views.KakaoCallbackView.as_view(), name='kakao_callback'),
-    path('kakao/login/finish/', views.KakaoLoginToDjango.as_view(), name='kakao_login_todjango'),
+    path('kakao/login/finish/', views.KakaoLoginToDjango.as_view(),
+         name='kakao_login_todjango'),
 ]
--- backend/run.sh
+++ backend/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+pip install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 
