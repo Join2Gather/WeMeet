5b96cd992afabb9726ccc7382dddef6a80660878
ww8007 <wshmin@naver.com>
2021-10-03 20:10:21 +0900

    Fix: postTeamName error Authorization, content-type

--- frontend/src/lib/api/team.ts
+++ frontend/src/lib/api/team.ts
@@ -2,7 +2,12 @@ import axios from 'axios';
 import client from './client';
 import type { requestTeamAPI } from '../../interface';
 
-export const postTeamName = ({ user, id, name }: requestTeamAPI) => {
-	const data = JSON.stringify(name);
-	return client.post(`/users/${user}/profiles/${id}/clubs`, data);
+export const postTeamName = ({ user, id, name, token }: requestTeamAPI) => {
+	const headers = {
+		'Content-type': 'Application/json',
+		Authorization: `Token ${token}`,
+		Accept: '*/*',
+	};
+	const data = JSON.stringify({ name: encodeURIComponent(name) });
+	return client.post(`/users/${user}/profiles/${id}/clubs`, data, { headers });
 };
