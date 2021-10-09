2b431670c8feb893a881630554ce0d8df236f6bd
ww8007 <wshmin@naver.com>
2021-10-09 19:06:34 +0900

    Fix: login server url

--- frontend/src/screens/Login.tsx
+++ frontend/src/screens/Login.tsx
@@ -43,8 +43,8 @@ export default function Login() {
 	}, [name]);
 	const onPressSocial = useCallback(async (social: any) => {
 		setSocialModalVisible(true);
-		//setSource('http://api.dps0340.xyz/accounts/kakao/login');
-		setSource(`http://localhost:8000/accounts/kakao/login`);
+		setSource('http://api.dps0340.xyz/accounts/kakao/login');
+		//setSource(`http://localhost:8000/accounts/kakao/login`);
 	}, []);
 	const onCloseSocial = useCallback(async () => {
 		setSocialModalVisible(false);
@@ -83,7 +83,7 @@ export default function Login() {
 									]}
 									onPress={() => {
 										onPressSocial('');
-										// goTabNavigator();
+										//goTabNavigator();
 									}}
 								>
 									<Text style={styles.loginText}>카카오 로그인</Text>
