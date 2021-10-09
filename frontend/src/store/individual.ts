bec3d3cc9a0b9279e363faaa421905398c0abff3
ww8007 <wshmin@naver.com>
2021-10-09 19:13:13 +0900

    Feat: add individual store with slice 개인 페이지에 대한 정보를 담고 있음

---
+++ frontend/src/store/individual.ts
@@ -0,0 +1,65 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import createRequestSaga from '../hooks/createRequestSaga';
+import * as api from '../lib/api/individual';
+import { takeLatest } from 'redux-saga/effects';
+import { createAction } from 'redux-actions';
+import type {
+	default_days,
+	requestTeamAPI,
+	responseTeamAPI,
+	state_time,
+	team,
+	postImageAPI,
+	individual,
+	responseImageAPI,
+} from '../interface';
+
+const POST_IMAGE = 'individual/POST_IMAGE';
+
+export const postTeamName = createAction(
+	POST_IMAGE,
+	(data: postImageAPI) => data
+);
+
+const postImageSaga = createRequestSaga(POST_IMAGE, api.postImage);
+
+export function* individualSaga() {
+	yield takeLatest(POST_IMAGE, postImageSaga);
+}
+
+const initialState: individual = {
+	dates: {
+		sun: { time: [] },
+		mon: { time: [] },
+		tue: { time: [] },
+		thu: { time: [] },
+		wed: { time: [] },
+		fri: { time: [] },
+		sat: { time: [] },
+	},
+	error: '',
+};
+
+export const individualSlice = createSlice({
+	name: 'individual',
+	initialState,
+	reducers: {
+		POST_IMAGE_SUCCESS: (state, action: PayloadAction<responseImageAPI>) => {
+			state.dates.mon.time = action.payload.mon;
+			state.dates.tue.time = action.payload.tue;
+			state.dates.wed.time = action.payload.wed;
+			state.dates.thu.time = action.payload.thu;
+			state.dates.fri.time = action.payload.fri;
+			state.dates.sat.time = action.payload.sat;
+			state.dates.sun.time = action.payload.sun;
+		},
+		POST_IMAGE_FAILURE: (state, action: PayloadAction<any>) => {
+			state.error = action.payload;
+		},
+	},
+	extraReducers: {},
+});
+
+export const {} = individualSlice.actions;
+
+export default individualSlice.reducer;
