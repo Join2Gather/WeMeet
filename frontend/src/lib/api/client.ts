50a59ccb76c859835378f29cb4b5a84bbc98e4e7
ww8007 <wshmin@naver.com>
2021-10-09 23:30:15 +0900

    Feat: add local, serverURL

--- frontend/src/lib/api/client.ts
+++ frontend/src/lib/api/client.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const client = axios.create();
 
-client.defaults.baseURL = 'http://localhost:8000/';
+client.defaults.baseURL = 'http://api.dps0340.xyz/';
+//client.defaults.baseURL = 'http://localhost:8000/';
 
 // client.defaults.baseURL = '';
 // client.defaults.withCredentials = true;
