cc639f7aec5c1ca77e8f97683aef5a32c97c2966
ww8007 <wshmin@naver.com>
2021-10-11 23:46:37 +0900

    Feat: add setStart, EndHour and Min

--- frontend/src/store/timetable.ts
+++ frontend/src/store/timetable.ts
@@ -7,23 +7,24 @@ import type {
 	default_days,
 	requestTeamAPI,
 	responseTeamAPI,
+	selectDay,
 	state_time,
 	team,
 	timetable,
 } from '../interface';
 
-const POST_TEAM = 'team/POST_TEAM';
+// const POST_TEAM = 'team/POST_TEAM';
 
-export const postTeamName = createAction(
-	POST_TEAM,
-	(data: requestTeamAPI) => data
-);
+// export const postTeamName = createAction(
+// 	POST_TEAM,
+// 	(data: requestTeamAPI) => data
+// );
 
-const postTeamSaga = createRequestSaga(POST_TEAM, api.postTeamName);
+// const postTeamSaga = createRequestSaga(POST_TEAM, api.postTeamName);
 
-export function* teamSaga() {
-	yield takeLatest(POST_TEAM, postTeamSaga);
-}
+// export function* teamSaga() {
+// 	yield takeLatest(POST_TEAM, postTeamSaga);
+// }
 
 const initialState: timetable = {
 	dates: {
@@ -35,6 +36,18 @@ const initialState: timetable = {
 		fri: { day: 'fri', times: [] },
 		sat: { day: 'sat', times: [] },
 	},
+	startTime: 0.0,
+	endTime: 0.0,
+	selectTime: {
+		sun: [],
+		mon: [],
+		tue: [],
+		thu: [],
+		wed: [],
+		fri: [],
+		sat: [],
+	},
+	day: '',
 };
 
 export const timetableSlice = createSlice({
@@ -44,10 +57,31 @@ export const timetableSlice = createSlice({
 		cloneDates: (state, action: PayloadAction<default_days>) => {
 			state.dates = action.payload;
 		},
+		setStartHour: (state, action: PayloadAction<number>) => {
+			state.startTime = action.payload;
+		},
+		setEndHour: (state, action: PayloadAction<number>) => {
+			state.startTime = action.payload;
+		},
+		setStartMin: (state, action: PayloadAction<number>) => {
+			state.startTime = state.startTime + action.payload / 100;
+		},
+		setEndMin: (state, action: PayloadAction<number>) => {
+			state.startTime = state.endTime + action.payload / 100;
+		},
+		pushSelectStart: (state, action: PayloadAction<number>) => {
+			state.selectTime[state.day].push(action.payload);
+		},
+		pushSelectEnd: (state, action: PayloadAction<number>) => {
+			for (let i = state.startTime + 1; i < state.endTime; i++) {
+				state.selectTimes.push(i);
+			}
+		},
 	},
 	extraReducers: {},
 });
 
-export const { cloneDates } = timetableSlice.actions;
+export const { cloneDates, setStartHour, setEndHour, setStartMin, setEndMin } =
+	timetableSlice.actions;
 
 export default timetableSlice.reducer;
