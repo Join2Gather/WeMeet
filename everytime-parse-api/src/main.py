df4adf43e6f3ccb6efeee99969c3274c83c8aa3b
jiho lee <optional.int@kakao.com>
2021-10-21 02:13:20 +0900

    feat: add parse image api

--- everytime-parse-api/src/main.py
+++ everytime-parse-api/src/main.py
@@ -3,7 +3,9 @@ if __name__ == '__main__':
     import os
     path.append(os.path.dirname(__file__))
 
-from parse import get_result
+from fastapi.datastructures import UploadFile
+from fastapi.params import File
+from parse import get_result, parse_img
 from fastapi import FastAPI, Response
 from fastapi.encoders import jsonable_encoder as to_json
 from fastapi.responses import JSONResponse
@@ -19,6 +21,15 @@ async def parse_everytime(id: str, password: str) -> None:
     if 'error' in result.keys():
         status_code = 400
     return JSONResponse(result, status_code=status_code)
+
+
+@app.post("/image")
+async def parse_image(image: UploadFile = File(...)) -> Response:
+    if not image.content_type.startswith('image'):
+        return JSONResponse(to_json({'error': 'file is not image file.'}), status_code=400)
+    result = parse_img(img_file=image.file)
+    return JSONResponse(to_json({'result': result}), status_code=200)
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8009)
--- everytime-parse-api/src/parse.py
+++ everytime-parse-api/src/parse.py
@@ -1,3 +1,8 @@
+from typing import BinaryIO, List, Tuple, Union
+import pprint
+import math
+import cv2
+import numpy as np
 import aiohttp
 from lxml import etree
 from datetime import datetime
@@ -89,3 +94,112 @@ async def get_result(id: str, password: str) -> dict:
         year, semester = await get_year_semester(session)
         table_id = await get_table_id(session, year, semester)
         return await get_times(session, table_id)
+
+
+def normal_round(n):
+    if n - math.floor(n) < 0.5:
+        return math.floor(n)
+    return math.ceil(n)
+
+
+def parse_img(img_url: str = "", img_file: BinaryIO = None, debug: bool = True):
+    if img_url:
+        img: np.ndarray = cv2.imread(img_url)
+    elif img_file:
+        file_bytes = np.fromstring(img_file.read(), dtype=np.uint8)
+        img: np.ndarray = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    else:
+        return []
+    img_y, img_x, _ = img.shape
+    print(img_x, img_y)
+
+    width = 960
+    r = width / float(img_x)
+    dim = (width, int(img_y * r))
+
+    img = cv2.resize(img, dim)
+
+    img_y, img_x, _ = img.shape
+
+    reshaped: np.ndarray = np.reshape(img, (-1, 3))  # 2d rgb to 1d rgb
+    colors: np.ndarray = np.unique(reshaped, axis=0)
+
+    result_img = img.copy()
+    result = {}
+
+    week = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']
+
+    for day in week:
+        result[day] = []
+
+    padding_y = 80.0
+    padding_x = 70.0
+
+    one_hour_y = 180.0
+    one_day_x = 177
+
+    initial_hour = 9.0
+
+    for color in colors:
+        if len(set(color.tolist())) == 1:
+            continue
+        mask_color = cv2.inRange(img, color, color)
+        contours, _ = cv2.findContours(
+            mask_color, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        for contour in contours:
+            x, y, w, h = map(int, cv2.boundingRect(contour))
+            if x == 0 or y == 0:
+                continue
+            if 50 > w or 50 > h:
+                continue
+
+            starting_time = initial_hour + (y - padding_y) / one_hour_y
+            end_time = starting_time + (h / one_hour_y)
+            starting_time = round(starting_time, 2)
+            end_time = round(end_time, 2)
+
+            starting_hour = int(starting_time // 1)
+            end_hour = int(end_time // 1)
+
+            starting_minutes = 0
+            end_minutes = 0
+
+            if starting_time % 1.0:
+                starting_minutes = normal_round(
+                    (starting_time % 1.0) * 60.0)  # avoid banker's rounding
+            if end_time % 1.0:
+                end_minutes = normal_round((end_time % 1.0) * 60.0)
+                if end_minutes >= 59:  # avoid IEEE 754 error
+                    end_hour += 1
+                    end_minutes = 0
+
+            day_num = round((x - padding_x) / one_day_x)
+            day = week[day_num]
+
+            result[day].append({'starting_hour': starting_hour, 'starting_minutes': starting_minutes,
+                                'end_hour': end_hour, 'end_minutes': end_minutes})
+
+            result_img = cv2.rectangle(
+                result_img, (x, y), (x+w, y+h), (0, 0, 255), 2)
+
+    for day in week:
+        result[day].sort(key=lambda x: [v for v in x.values()])
+
+    if debug:
+        printer = pprint.PrettyPrinter(2)
+        printer.pprint(result)
+        for day, idx in result.items():
+            print(
+                f"{day}요일 수업 시작 시간: {idx}")
+
+        cv2.imshow('img', result_img)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+
+    return result
+
+
+if __name__ == '__main__':
+    parse_img(img_url='./assets/everytime.png', debug=True)
