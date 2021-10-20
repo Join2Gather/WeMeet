5228c0bae5b266ea92a13599c1908cb696ad7735
jiho lee <optional.int@kakao.com>
2021-10-20 13:18:28 +0900

    feat: add everytime crawling

--- everytime-parse-api/src/main.py
+++ everytime-parse-api/src/main.py
@@ -3,22 +3,22 @@ if __name__ == '__main__':
     import os
     path.append(os.path.dirname(__file__))
 
-from typing import Optional
-from parse import parse_img
-from fastapi import FastAPI, File, UploadFile, Response
+from parse import get_result
+from fastapi import FastAPI, Response
 from fastapi.encoders import jsonable_encoder as to_json
 from fastapi.responses import JSONResponse
 import uvicorn
 
-app = FastAPI()
+app = FastAPI(docs_url='/swagger')
 
 
 @app.post("/everytime")
-async def parse_everytime(image: UploadFile = File(...)) -> Response:
-    if not image.content_type.startswith('image'):
-        return JSONResponse(to_json({'error': 'file is not image file.'}), status_code=400)
-    result = parse_img(img_file=image.file)
-    return JSONResponse(to_json({'result': result}), status_code=200)
+async def parse_everytime(id: str, password: str) -> None:
+    result = await get_result(id, password)
+    status_code = 200
+    if 'error' in result.keys():
+        status_code = 400
+    return JSONResponse(result, status_code=status_code)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8009)
