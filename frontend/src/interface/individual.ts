018aed572a0b076db5d54647902117d6b030c08a
ww8007 <wshmin@naver.com>
2021-10-31 23:20:59 +0900

    Types: add individualDates, evertTime, weeIndex

--- frontend/src/interface/individual.ts
+++ frontend/src/interface/individual.ts
@@ -1,6 +1,10 @@
+import type { make_days } from './timetable';
+
 export interface individual {
-	dates: individualTime;
+	individualDates: make_days[];
 	error: string;
+	everyTime: responseEveryTime;
+	weekIndex: Array<string>;
 }
 
 export interface postImageAPI {
@@ -30,4 +34,28 @@ export interface individualTime {
 	thu: { time: Array<string> };
 	fri: { time: Array<string> };
 	sat: { time: Array<string> };
+}
+
+export interface responseEveryTime {
+	sun: Array<responseTime>;
+	mon: Array<responseTime>;
+	tue: Array<responseTime>;
+	wed: Array<responseTime>;
+	thu: Array<responseTime>;
+	fri: Array<responseTime>;
+	sat: Array<responseTime>;
+	[prop: string]: responseTime[];
+}
+
+export interface responseTime {
+	starting_hours: number;
+	starting_minutes: number;
+	end_hours: number;
+	end_minutes: number;
+}
+
+export interface postEveryTimeAPI {
+	user: number;
+	id: number;
+	data: responseEveryTime;
 }
