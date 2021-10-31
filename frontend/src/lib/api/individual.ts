65e34dc9ced0388a26d6559e17902337f35ce89a
ww8007 <wshmin@naver.com>
2021-10-31 23:24:23 +0900

    Feat: postEveryTime API

--- frontend/src/lib/api/individual.ts
+++ frontend/src/lib/api/individual.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
-import type { postImageAPI, loginEveryTimeAPI } from '../../interface';
+import type {
+	postImageAPI,
+	loginEveryTimeAPI,
+	postEveryTimeAPI,
+} from '../../interface';
 import FormData from 'form-data';
 import { LOGIN_URL, IMAGE_URL } from 'react-native-dotenv';
 export const postImage = ({ image, token }: postImageAPI) => {
@@ -23,4 +27,8 @@ export const postImage = ({ image, token }: postImageAPI) => {
 
 export const loginEveryTime = ({ id, password }: loginEveryTimeAPI) => {
 	return axios.post(`${LOGIN_URL}?id=${id}&password=${password}`);
+};
+
+export const postEveryTime = ({ user, id }: postEveryTimeAPI) => {
+	return axios.post(`/users/${user}/profiles/${id}/everytime`);
 };
