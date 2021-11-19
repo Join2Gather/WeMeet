d5a9be40ed35ead6232dd1d09f7eb029c660175d
ww8007 <wshmin@naver.com>
2021-11-19 21:26:48 +0900

    Feat: add util hexToRGB function hex 값을 RGB로 변경

---
+++ frontend/src/lib/util/hexToRGB.ts
@@ -0,0 +1,10 @@
+export function hexToRGB(hex: string) {
+	let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+	return result
+		? {
+				r: parseInt(result[1], 16),
+				g: parseInt(result[2], 16),
+				b: parseInt(result[3], 16),
+		  }
+		: null;
+}
