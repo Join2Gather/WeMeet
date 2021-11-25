60f635e9742b978a846a6fff1fa80ce388dc12ac
ww8007 <wshmin@naver.com>
2021-11-25 17:27:22 +0900

    Fix: date object parsing error

--- frontend/src/screens/Login/SocialWebview.tsx
+++ frontend/src/screens/Login/SocialWebview.tsx
@@ -1,9 +1,10 @@
 import { useNavigation } from '@react-navigation/native';
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { WebView } from 'react-native-webview';
 import { useDispatch } from 'react-redux';
+import { kakaoLogin } from '../../store/individual';
 import { getSocialLogin } from '../../store/login';
-
+import type { kakaoLoginAPI } from '../../interface';
 // import { useAsyncStorage } from '@react-native-community/async-storage';
 // const { setItem } = useAsyncStorage('토큰 key 값');
 
@@ -12,19 +13,26 @@ export function SocialWebview({ closeSocialModal, source }: any) {
 	const navigation = useNavigation();
 	const INJECTED_JAVASCRIPT =
 		'(function() {if(window.document.getElementsByTagName("pre").length>0){window.document.getElementsByTagName("pre")[0].style.display = "none";window.ReactNativeWebView.postMessage((window.document.getElementsByTagName("pre")[0].innerHTML));}})();';
+	let data: kakaoLoginAPI;
 
+	useEffect(() => {
+		return () => {
+			dispatch(kakaoLogin(data.kakaoDates));
+		};
+	}, []);
 	const _handleMessage = async (event: any) => {
 		closeSocialModal();
 
 		let result = JSON.parse(event.nativeEvent.data);
-
-		const data = {
+		let date = result.profiles[0].dates.filter((day: any) => day.club === null);
+		date = date[0];
+		data = {
 			token: result.access_token,
 			name: result.profiles[0].name,
 			user: result.profiles[0].user,
 			id: result.profiles[0].id,
 			clubs: result.profiles[0].clubs,
-			kakaoDates: result.profiles[0].dates,
+			kakaoDates: date,
 		};
 		// let userToken = result.Authorization;
 
