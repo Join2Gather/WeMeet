fd651f32e642ebcfe512a2e0ce0a346b7d2aa1c9
ww8007 <wshmin@naver.com>
2021-11-29 18:54:22 +0900

    Feat: add Button component for refactor

---
+++ frontend/src/lib/util/Button.tsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+import { Colors } from 'react-native-paper';
+
+interface props {
+	buttonNumber: number;
+	buttonText: string;
+	secondButtonText?: string;
+	onPressFunction?: () => void;
+	secondOnPressFunction?: () => void;
+	onPressWithParam?: (pressParam: any) => void;
+	secondOnPressWithParam?: (secondParam: any) => void;
+	pressParam?: any;
+	secondParam?: any;
+}
+
+export function Button({
+	buttonNumber,
+	buttonText,
+	secondButtonText,
+	onPressFunction,
+	secondOnPressFunction,
+	onPressWithParam,
+	secondOnPressWithParam,
+	pressParam,
+	secondParam,
+}: props) {
+	return (
+		<>
+			{buttonNumber === 1 && (
+				<View style={styles.buttonRowView}>
+					<TouchableHighlight
+						activeOpacity={0.1}
+						underlayColor={Colors.grey200}
+						style={styles.closeButtonStyle}
+						onPress={() => {
+							onPressFunction && onPressFunction();
+							onPressWithParam && pressParam && onPressWithParam(pressParam);
+						}}
+					>
+						<Text style={styles.buttonText}>{buttonText}</Text>
+					</TouchableHighlight>
+				</View>
+			)}
+			{buttonNumber === 2 && (
+				<View style={styles.buttonWithTwoView}>
+					<TouchableHighlight
+						activeOpacity={0.1}
+						underlayColor={Colors.grey200}
+						style={[styles.twoButtonStyle]}
+						onPress={() => {
+							onPressFunction && onPressFunction();
+							onPressWithParam && pressParam && onPressWithParam(pressParam);
+						}}
+					>
+						<Text style={[styles.buttonText, { borderBottomRightRadius: 13 }]}>
+							{buttonText}
+						</Text>
+					</TouchableHighlight>
+					<View
+						style={{
+							height: '100%',
+							borderWidth: 0.3,
+						}}
+					/>
+					<TouchableHighlight
+						activeOpacity={0.1}
+						underlayColor={Colors.grey200}
+						style={[styles.twoButtonStyle, { borderBottomRightRadius: 13 }]}
+						onPress={() => {
+							secondOnPressFunction && secondOnPressFunction();
+							secondOnPressWithParam &&
+								secondParam &&
+								secondOnPressWithParam(secondParam);
+						}}
+					>
+						<Text style={styles.buttonText}>{secondButtonText}</Text>
+					</TouchableHighlight>
+				</View>
+			)}
+		</>
+	);
+}
+
+const styles = StyleSheet.create({
+	buttonRowView: {
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignContent: 'center',
+		alignSelf: 'center',
+		marginTop: 10,
+		marginBottom: -14,
+	},
+	closeButtonStyle: {
+		padding: 15,
+		width: '80%',
+		height: '100%',
+		borderRadius: 13,
+	},
+	twoButtonStyle: {
+		padding: 15,
+		width: '56%',
+		height: '100%',
+		paddingLeft: 10,
+		paddingRight: 10,
+	},
+	buttonWithTwoView: {
+		flexDirection: 'row',
+		marginBottom: -20,
+	},
+	buttonText: {
+		textAlign: 'center',
+		fontFamily: 'NanumSquareR',
+	},
+});
