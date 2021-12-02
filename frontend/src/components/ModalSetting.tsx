428780969725849bd6cf0c86049c0d36e1607155
ww8007 <wshmin@naver.com>
2021-12-02 17:14:18 +0900

    Feat: add invite team icon add get snapShot logic, icon

--- frontend/src/components/ModalSetting.tsx
+++ frontend/src/components/ModalSetting.tsx
@@ -18,7 +18,10 @@ import { hexToRGB } from '../lib/util/hexToRGB';
 import { ColorPicker, fromHsv } from 'react-native-color-picker';
 import { changeColor, setModalMode } from '../store/team';
 import { Button } from '../lib/util/Button';
-import { getSnapShot } from '../store/timetable';
+import { changeTimetableColor, getSnapShot } from '../store/timetable';
+import { getUserMe, makeGroupColor } from '../store/login';
+import { useNavigation } from '@react-navigation/core';
+import { make60 } from '../interface';
 
 const screen = Dimensions.get('screen');
 
@@ -30,6 +33,10 @@ interface props {
 	token: string;
 	color: string;
 	uri?: string;
+	loadingChangeColor: [prop: string];
+	createdDate: string;
+	name: string;
+	onShareURI: () => void;
 }
 
 export function ModalSetting({
@@ -40,6 +47,9 @@ export function ModalSetting({
 	token,
 	color,
 	uri,
+	createdDate,
+	name,
+	onShareURI,
 }: props) {
 	const dispatch = useDispatch();
 	const [mode, setMode] = useState('initial');
@@ -50,6 +60,18 @@ export function ModalSetting({
 		g: 0,
 		b: 0,
 	});
+	const navigation = useNavigation();
+	useEffect(() => {
+		if (mode === 'loading') {
+			setTimeout(() => {
+				setMode('success');
+			}, 500);
+		} else if (mode === 'loadingSave') {
+			setTimeout(() => {
+				setMode('snapShot');
+			}, 500);
+		}
+	}, [mode]);
 
 	// useCallback
 	useEffect(() => {
@@ -58,16 +80,34 @@ export function ModalSetting({
 	}, [color]);
 	const onPressCloseButton = useCallback(() => {
 		setSettingModalVisible(false);
+		setMode('initial');
 	}, []);
 
 	const onPressSetMode = useCallback((mode: string) => {
 		setMode(mode);
 	}, []);
+	// 팀 색 변경
 	const onPressChangeColor = useCallback(() => {
 		uri && dispatch(changeColor({ color: pickColor, id, token, user, uri }));
+		dispatch(getUserMe({ id, token, user }));
+		dispatch(makeGroupColor(pickColor));
+
+		setMode('loading');
 	}, [pickColor]);
 	const onPressGetSnapShot = useCallback(() => {
 		uri && dispatch(getSnapShot({ id, token, user, uri }));
+		setMode('loadingSave');
+	}, []);
+	const onFinishChangeColor = useCallback(() => {
+		dispatch(changeTimetableColor(pickColor));
+		setPickColor(Colors.red500);
+		setSettingModalVisible(false);
+		setMode('initial');
+	}, [pickColor]);
+	const goSnapShotPage = useCallback(() => {
+		setSettingModalVisible(false);
+		setModalMode('initial');
+		navigation.navigate('SnapShot', { name, color });
 	}, []);
 	return (
 		<Modal
@@ -118,10 +158,10 @@ export function ModalSetting({
 										<View style={styles.rowView}>
 											<Font5Icon
 												name="palette"
-												size={23}
+												size={21}
 												color={`rgba(${RGBColor.r}, ${RGBColor.g}, ${RGBColor.b}, 0.6)`}
 											/>
-											<Text style={styles.touchText}>팀 색상 변경</Text>
+											<Text style={styles.touchText}> 팀 색상 변경</Text>
 											<View style={styles.iconView}>
 												<Font5Icon
 													name="angle-right"
@@ -131,7 +171,36 @@ export function ModalSetting({
 											</View>
 										</View>
 									</TouchableHighlight>
-									<View style={{ height: 10 }} />
+									<View style={styles.blankView} />
+									<TouchableHighlight
+										activeOpacity={1}
+										underlayColor={Colors.grey300}
+										onPress={onShareURI}
+										style={styles.touchButtonStyle}
+									>
+										<View style={styles.rowView}>
+											<Font5Icon
+												name="user-plus"
+												size={20}
+												color={`rgba(${RGBColor.r}, ${RGBColor.g}, ${RGBColor.b}, 0.6)`}
+											/>
+											<Text style={styles.touchText}>팀원 초대</Text>
+											<View style={styles.iconView}>
+												<Font5Icon
+													name="angle-right"
+													size={19}
+													color={Colors.black}
+												/>
+											</View>
+										</View>
+									</TouchableHighlight>
+								</View>
+							</View>
+							<View style={styles.blankView} />
+							<Text style={styles.titleText}>개인 설정</Text>
+							<View style={styles.blankView} />
+							<View style={[styles.backgroundView]}>
+								<View style={styles.columnView}>
 									<TouchableHighlight
 										activeOpacity={1}
 										underlayColor={Colors.grey300}
@@ -180,17 +249,12 @@ export function ModalSetting({
 							>
 								<ColorPicker
 									onColorSelected={(color) => alert(`Color selected: ${color}`)}
+									onColorChange={(color) => setPickColor(fromHsv(color))}
 									style={{ flex: 1 }}
 									hideSliders={true}
 								/>
 							</View>
-							<View
-								style={{
-									borderWidth: 0.3,
-									width: '113%',
-									marginTop: 20,
-								}}
-							/>
+							<View style={styles.buttonOverLine} />
 							<Button
 								buttonNumber={2}
 								buttonText={'이전'}
@@ -198,6 +262,72 @@ export function ModalSetting({
 								onPressWithParam={() => onPressSetMode('initial')}
 								pressParam="initial"
 								secondOnPressFunction={() => onPressChangeColor()}
+							/>
+						</>
+					)}
+					{mode === 'snapShot' && (
+						<>
+							<View style={styles.blankView} />
+							<Text style={styles.titleText}>저장 시간 불러오기</Text>
+
+							<View style={styles.blankView} />
+							<View style={[styles.backgroundView]}>
+								<View style={styles.columnView}>
+									<TouchableHighlight
+										activeOpacity={1}
+										underlayColor={Colors.grey300}
+										onPress={goSnapShotPage}
+										style={styles.touchButtonStyle}
+									>
+										<View style={styles.rowView}>
+											<Font5Icon
+												name="save"
+												size={23}
+												color={`rgba(${RGBColor.r}, ${RGBColor.g}, ${RGBColor.b}, 0.6)`}
+											/>
+											<Text style={styles.touchText}>{createdDate}</Text>
+											<View style={styles.iconView}>
+												<Font5Icon
+													name="angle-right"
+													size={19}
+													color={Colors.black}
+												/>
+											</View>
+										</View>
+									</TouchableHighlight>
+								</View>
+							</View>
+							<Button buttonNumber={1} buttonText={'이전'} />
+						</>
+					)}
+					{mode === 'loading' ||
+						(mode === 'loadingSave' && (
+							<>
+								<View style={styles.blankView} />
+								<ActivityIndicator size={'large'} color={color} />
+								<View style={styles.blankView} />
+							</>
+						))}
+					{mode === 'success' && (
+						<>
+							<View style={styles.blankView} />
+							<View style={styles.rowView}>
+								<Font5Icon
+									name="check-circle"
+									size={19}
+									color={Colors.green500}
+								/>
+								<Text style={styles.touchText}>
+									{' '}
+									변경 사항이 저장 되었습니다
+								</Text>
+							</View>
+							<View style={styles.blankView} />
+							<View style={styles.buttonOverLine} />
+							<Button
+								buttonNumber={1}
+								buttonText="확인"
+								onPressFunction={onFinishChangeColor}
 							/>
 						</>
 					)}
@@ -253,7 +383,7 @@ const styles = StyleSheet.create({
 		},
 		shadowOpacity: 0.21,
 		shadowRadius: 1.0,
-		width: '90%',
+		width: screen.width * 0.9,
 	},
 	touchText: {
 		fontSize: 14,
@@ -280,5 +410,11 @@ const styles = StyleSheet.create({
 	touchButtonStyle: {
 		padding: 5,
 		borderRadius: 10,
+	},
+	buttonOverLine: {
+		borderWidth: 0.4,
+		width: screen.width * 0.9,
+		marginTop: 20,
+		borderColor: Colors.black,
 	},
 });
