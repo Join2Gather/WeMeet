0a89c87ec1f2ddb8e2283fd26c0321e024b82ecf
ww8007 <wshmin@naver.com>
2021-12-04 22:51:40 +0900

    Fix: Timetable comopnent render bug

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -3,7 +3,7 @@ export * from './Avatar';
 export * from './LeftSwipe';
 export * from './LeftRightNavigation';
 export * from './ModalInput';
-export * from './Timetable';
+// export * from './Timetable';
 export * from './ModalMinute';
 export * from './ModalSelect';
 export * from './Spinner';
