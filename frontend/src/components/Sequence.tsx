4a5e962c393add3d19737bcbe056c1bbf8bb075c
ww8007 <wshmin@naver.com>
2021-12-09 16:02:37 +0900

    Feat: add Sequence comopnent for progress 진행 상태를 나타내기 위한 시퀀스 컴포넌트 추가

---
+++ frontend/src/components/Sequence.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Colors } from 'react-native-paper';
+
+interface props {
+	currentNumber: number;
+	mode: number[];
+	color: string;
+}
+
+export function Sequence({ currentNumber, mode, color }: props) {
+	console.log(currentNumber, mode, color);
+	return (
+		<View style={styles.rowView}>
+			{mode.map((c, idx) => (
+				<View style={styles.rowView} key={idx}>
+					{idx !== mode.length - 1 && (
+						<View style={styles.rowView}>
+							<View
+								style={[
+									styles.circle,
+									{
+										backgroundColor:
+											idx === currentNumber ? color : Colors.white,
+									},
+								]}
+							/>
+							<View style={styles.rowLine} />
+						</View>
+					)}
+
+					{idx === mode.length - 1 && (
+						<View
+							style={[
+								styles.circle,
+								{
+									backgroundColor: idx === currentNumber ? color : Colors.white,
+								},
+							]}
+						/>
+					)}
+				</View>
+			))}
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	rowView: {
+		flexDirection: 'row',
+		justifyContent: 'center',
+	},
+	circle: {
+		width: 10 * 2,
+		height: 10 * 2,
+		borderRadius: 10,
+
+		borderWidth: 3,
+		// marginLeft: 15,
+		// marginRight: 15,
+	},
+	rowLine: {
+		width: 20,
+		height: 1,
+		backgroundColor: Colors.black,
+		alignSelf: 'center',
+	},
+});
