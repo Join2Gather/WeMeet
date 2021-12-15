32001d6ba40eea610cb27920154e29abc7104d78
ww8007 <wshmin@naver.com>
2021-12-15 11:20:22 +0900

    Feat: add makePostHomeDates, checkHomeIsBlank slices 시간 보내기 로직 위한 액션들 추가

--- frontend/src/store/individual.ts
+++ frontend/src/store/individual.ts
@@ -13,6 +13,7 @@ import type {
 import { Colors } from 'react-native-paper';
 import { useMakeTimeTableWith60 } from '../hooks';
 import { makeHomeTimetable } from '../lib/util';
+import { Alert } from 'react-native';
 
 const POST_IMAGE = 'individual/POST_IMAGE';
 const LOGIN_EVERYTIME = 'individual/LOGIN_EVERYTIME';
@@ -64,6 +65,11 @@ const initialState: individual = {
 		minute: 0,
 	},
 	inTimeMode: '',
+	dayIdx: 0,
+	dayString: '',
+	isHomeTimePicked: false,
+	postHomePrepare: false,
+	postHomeSuccess: false,
 };
 
 export const individualSlice = createSlice({
@@ -81,6 +87,7 @@ export const individualSlice = createSlice({
 				confirmClubs: Array<string>;
 			}>
 		) => {
+			// state.individualDates = defaultDatesWith60;
 			state.confirmClubs = action.payload.confirmClubs;
 			state.confirmDatesTimetable = action.payload.confirmDatesTimetable;
 			state.confirmDatesTimetable?.length &&
@@ -132,6 +139,7 @@ export const individualSlice = createSlice({
 		initialIndividualTimetable: (state) => {
 			state.individualDates = defaultDatesWith60;
 			makeHomeTimetable(state);
+			state.postHomeSuccess = false;
 		},
 		kakaoLogin: (state, action: PayloadAction<any>) => {
 			if (state.cloneDateSuccess) {
@@ -191,15 +199,19 @@ export const individualSlice = createSlice({
 		},
 		POST_EVERYTIME_SUCCESS: (state, action: PayloadAction<any>) => {
 			state.loginSuccess = false;
+			state.postHomePrepare = false;
+			state.postHomeSuccess = true;
 		},
 		POST_EVERYTIME_FAILURE: (state, action: PayloadAction<any>) => {
 			state.error = action.payload;
+			state.postHomeSuccess = false;
 		},
 		setInStartTime: (
 			state,
 			action: PayloadAction<{ hour: number; min: number }>
 		) => {
 			const { hour, min } = action.payload;
+
 			state.startTime.hour = hour;
 			state.startTime.minute = min;
 		},
@@ -211,25 +223,18 @@ export const individualSlice = createSlice({
 			state.endTime.hour = hour;
 			state.endTime.minute = min;
 		},
+		initialTimeMode: (state) => {
+			state.inTimeMode = '';
+		},
 		checkInMode: (
 			state,
-			action: PayloadAction<{ time: number; idx: number }>
+			action: PayloadAction<{ time: number; idx: number; day: string }>
 		) => {
-			const { time, idx } = action.payload;
+			const { time, idx, day } = action.payload;
+			state.dayString = day;
 			state.inTimeMode = '';
+			state.dayIdx = idx;
 			let modeSelect = [
-				{
-					count: 0,
-					content: 'individual',
-				},
-				{
-					count: 0,
-					content: 'other',
-				},
-				{
-					count: 0,
-					content: 'everyTime',
-				},
 				{
 					count: 0,
 					content: 'normal',
@@ -238,18 +243,57 @@ export const individualSlice = createSlice({
 					count: 0,
 					content: 'team',
 				},
+				{
+					count: 0,
+					content: 'home',
+				},
 			];
 			state.individualDates[idx].times[time].forEach((t) => {
-				modeSelect.forEach((mode) => mode.content === t.mode && mode.count++);
+				modeSelect.forEach((mode) => {
+					mode.content === t.mode && mode.count++;
+				});
 			});
 			modeSelect.sort((a, b) => b.count - a.count);
-			console.log(modeSelect);
-			modeSelect.forEach((mode) => {
-				if (mode.count) {
-					state.inTimeMode += mode.content;
-				}
-			});
+
+			// modeSelect.forEach((mode) => {
+			// 	if (mode.count) {
+			// 		state.inTimeMode += mode.content;
+			// 	}
+			// });
+			state.inTimeMode = modeSelect[0].content;
 			console.log(state.inTimeMode);
+		},
+		makePostHomeDates: (state) => {
+			const data = {
+				starting_hours: state.startTime.hour,
+				starting_minutes: state.startTime.minute,
+				end_hours: state.endTime.hour,
+				end_minutes: state.endTime.minute,
+			};
+			state.everyTime[state.dayString] = [
+				...state.everyTime[state.dayString],
+				data,
+			];
+			state.postHomePrepare = true;
+		},
+		checkHomeIstBlank: (state) => {
+			let isNonColor = 0;
+			const dayIdx = state.dayIdx;
+			const endHour = state.endTime.hour;
+			state.isHomeTimePicked = false;
+			state.individualDates[dayIdx].times[endHour].forEach((t) => {
+				t.mode !== 'normal' && isNonColor++;
+			});
+
+			if (isNonColor !== 0) {
+				Alert.alert('알림', '이미 지정된 시간 입니다', [
+					{
+						text: '확인',
+						onPress: () => {},
+					},
+				]);
+				state.isHomeTimePicked = true;
+			}
 		},
 	},
 	extraReducers: {},
@@ -264,6 +308,9 @@ export const {
 	setInEndTime,
 	setInStartTime,
 	checkInMode,
+	makePostHomeDates,
+	initialTimeMode,
+	checkHomeIstBlank,
 } = individualSlice.actions;
 
 export default individualSlice.reducer;
