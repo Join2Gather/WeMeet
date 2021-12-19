1cb70bd950433b780ab1095e62a059e0224eb69e
ww8007 <wshmin@naver.com>
2021-12-19 16:05:17 +0900

    Fix: use axios to client made with default client

--- frontend/src/lib/api/timetable.ts
+++ frontend/src/lib/api/timetable.ts
@@ -1,5 +1,5 @@
 import client from './client';
-import axios from 'axios';
+
 import type {
 	requestGroupDatesAPI,
 	requestIndividualDatesAPI,
@@ -19,12 +19,9 @@ export const getIndividualDates = ({
 	token,
 }: requestIndividualDatesAPI) => {
 	const headers = makeHeader(token);
-	return client.get(
-		`${API_URL}users/${user}/profiles/${id}/clubs/${uri}/individual`,
-		{
-			headers,
-		}
-	);
+	return client.get(`users/${user}/profiles/${id}/clubs/${uri}/individual`, {
+		headers,
+	});
 };
 
 // 팀 시간 받아오기
@@ -35,12 +32,9 @@ export const getGroupDates = ({
 	token,
 }: requestGroupDatesAPI) => {
 	const headers = makeHeader(token);
-	return axios.get(
-		`${API_URL}users/${user}/profiles/${id}/clubs/${uri}/group`,
-		{
-			headers,
-		}
-	);
+	return client.get(`users/${user}/profiles/${id}/clubs/${uri}/group`, {
+		headers,
+	});
 };
 
 // 개인 시간 보내기
@@ -54,13 +48,9 @@ export const postIndividualTime = ({
 	const headers = makeHeader(token);
 	const data = JSON.stringify(dates);
 
-	return axios.post(
-		`${API_URL}users/${user}/profiles/${id}/clubs/${uri}`,
-		data,
-		{
-			headers,
-		}
-	);
+	return client.post(`users/${user}/profiles/${id}/clubs/${uri}`, data, {
+		headers,
+	});
 };
 
 // 그룹 시간 개별 확정
@@ -68,8 +58,8 @@ export const postConfirm = ({ user, id, uri, date, token }: postConfirmAPI) => {
 	const headers = makeHeader(token);
 	const data = JSON.stringify(date);
 
-	return axios.post(
-		`${API_URL}users/${user}/profiles/${id}/clubs/${uri}/confirm/ok`,
+	return client.post(
+		`users/${user}/profiles/${id}/clubs/${uri}/confirm/ok`,
 		data,
 		{
 			headers,
@@ -81,18 +71,17 @@ export const postConfirm = ({ user, id, uri, date, token }: postConfirmAPI) => {
 export const getSnapShot = ({ id, uri, user, token }: getSnapShotAPI) => {
 	const headers = makeHeader(token);
 
-	return axios.get(
-		`${API_URL}users/${user}/profiles/${id}/clubs/${uri}/snapshot`,
-		{ headers }
-	);
+	return client.get(`users/${user}/profiles/${id}/clubs/${uri}/snapshot`, {
+		headers,
+	});
 };
 
 // 스냅샷 생성
 export const confirmSnapShot = ({ id, uri, user, token }: getSnapShotAPI) => {
 	const headers = makeHeader(token);
 	const data = null;
-	return axios.post(
-		`${API_URL}users/${user}/profiles/${id}/clubs/${uri}/confirm`,
+	return client.post(
+		`users/${user}/profiles/${id}/clubs/${uri}/confirm`,
 		data,
 		{ headers }
 	);
@@ -102,8 +91,8 @@ export const confirmSnapShot = ({ id, uri, user, token }: getSnapShotAPI) => {
 export const revertConfirm = ({ id, uri, user, token }: getSnapShotAPI) => {
 	const headers = makeHeader(token);
 	const data = null;
-	return axios.post(
-		`${API_URL}users/${user}/profiles/${id}/clubs/${uri}/confirm/revert`,
+	return client.post(
+		`users/${user}/profiles/${id}/clubs/${uri}/confirm/revert`,
 		data,
 		{ headers }
 	);
