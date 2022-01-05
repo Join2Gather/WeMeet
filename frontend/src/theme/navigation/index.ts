32b6ccd4300a1f9dca1e7ac86852cd37c2cc74cf
ww8007 <wshmin@naver.com>
2022-01-05 22:34:03 +0900

    Feat: add export * from TouchHeaderIconView, CloseButton from theme

--- frontend/src/theme/navigation/index.ts
+++ frontend/src/theme/navigation/index.ts
@@ -6,3 +6,5 @@ export * from './Text';
 export * from './MaterialCommunityIcon';
 export * from './TextInput';
 export * from './TopBar';
+export * from './TouchHeaderIconView';
+export * from './ClosButton';
