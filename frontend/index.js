4a8f7504d95e45850ff079bb505059d94647f82d
ww8007 <wshmin@naver.com>
2022-01-28 21:17:33 +0900

    Feat: add background notification setting

--- frontend/index.js
+++ frontend/index.js
@@ -1,11 +1,17 @@
 import 'expo-dev-client';
-
+import messaging from '@react-native-firebase/messaging';
+import { AppRegistry } from 'react-native';
 import 'react-native-gesture-handler';
 import { registerRootComponent } from 'expo';
 
 import App from './App';
 
+messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+	console.log('Message handled in the background!', remoteMessage);
+});
+
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
+AppRegistry.registerComponent('app', () => App);
 registerRootComponent(App);
